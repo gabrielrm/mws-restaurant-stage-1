40699932181a25e3798f97c6aa66b249256414d6
Gabriel Radislav Mitin <gabriel.mitin@gmail.com>
2018-07-15 13:49:58 +0300

    Style reviewers name then mod js

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -150,6 +150,8 @@ createReviewHTML = (review) => {
   const li = document.createElement('li');
   const name = document.createElement('p');
   name.innerHTML = review.name;
+  // for css font transformation
+  name.className = "restaurant-reviewer"
   li.appendChild(name);
 
   const date = document.createElement('p');
